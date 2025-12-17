8037c8a8a44c4f9d4ca1028a5f87e07652085290
Raqib <raqibismail.work@gmail.com>
2025-12-18 07:28:51 +0800

    Update theme

--- lua/raqibismail/plugins/colors.lua
+++ lua/raqibismail/plugins/colors.lua
@@ -4,9 +4,33 @@ end
 
 return {
     {
-        "folke/tokyonight.nvim",
+        "ellisonleao/gruvbox.nvim",
         config = function()
-            vim.cmd.colorscheme("tokyonight-night")
+            -- Default options:
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = true,
+            })
+            vim.cmd.colorscheme("gruvbox")
             enable_transparency()
         end
     },
@@ -15,7 +39,7 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
             options = {
-                theme = "tokyonight",
+                theme = "gruvbox_dark",
             },
         },
     }
